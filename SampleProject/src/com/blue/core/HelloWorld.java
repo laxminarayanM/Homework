777e00ac5431b728b6f5f0d92f9c875dd0247c01
laxminarayanM <mittitu@gmail.com>
2015-07-12 01:08:12 +0200

    added additional comment.

--- SampleProject/src/com/blue/core/HelloWorld.java
+++ SampleProject/src/com/blue/core/HelloWorld.java
@@ -4,7 +4,7 @@ package com.blue.core;
 public class HelloWorld {
 	
 	public static void main (String[] args){
-		System.out.println("Hello World !!! ");
+		System.out.println("Hello World - Git here I come  !!! ");
 	}
 
 }
